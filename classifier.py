897f3a049ca60baefcda17c7e242572de1da93b7
Unguliant <ungulient@gmail.com>
2017-01-20 00:27:49 +0200

    18.1

--- classifier.py
+++ classifier.py
@@ -1,3 +1,27 @@
-from hw3_utils import abstract_classifier
+from hw3_utils import abstract_classifier, abstract_classifier_factory
+from euclidean_distance import euclidean_distance
+from collections import Counter
+
 
 class knn_classifier(abstract_classifier):
+    def __init__(self, k, data, labels):
+        self.k = k
+        self.data = data
+        self.labels = labels
+
+    def classify(self, features):
+        k_nearest_neighbors_indices = \
+            sorted(range(len(self.data)), key=lambda x: euclidean_distance(self.data[x], features))[:self.k]
+        count = Counter(self.labels[i] for i in k_nearest_neighbors_indices)
+        if count[0] > count[1]:
+            return 0
+        else:
+            return 1
+
+
+class knn_factory(abstract_classifier_factory):
+    def __init__(self, k):
+        self.k = k
+
+    def train(self, data, labels):
+        return knn_classifier(self.k, data, labels)
