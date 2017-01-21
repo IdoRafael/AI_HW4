6eb380860562d1eb610320605455ad142d2922c4
Unguliant <ungulient@gmail.com>
2017-01-21 18:04:19 +0200

    21.1

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,16 +1,5 @@
 from numpy import linalg as LA
-from math import sqrt
 
 
-# fastest
 def euclidean_distance(v0, v1):
     return LA.norm([a - b for a, b in zip(v0, v1)])
-
-
-# faster
-def euclidean_distance1(v0, v1):
-    return LA.norm([(v0[i] - v1[i]) for i in range(0,len(v0))])
-
-
-def euclidean_distance2(v0, v1):
-    return sqrt(sum([(v0[i] - v1[i])**2 for i in range(0,len(v0))]))
